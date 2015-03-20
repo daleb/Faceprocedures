57eacb7d0d9bf1ddf34ca555bab4f9c5cfa87a8e
Viji Kumar <vijikumar@carmatec.com>
2015-03-20 12:45:42 +0530

    for styles

--- app/controllers/control_controller.rb
+++ app/controllers/control_controller.rb
@@ -63,6 +63,15 @@ class ControlController < ApplicationController
     $experiment_status = params[:status]
     render json:{},status: :ok
   end
+  
+  def save_upload
+    name = params[:upload][:file].original_filename
+    directory = "public/csv"
+    path = File.join(directory, name)
+    File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
+    flash[:notice] = "File uploaded"
+    redirect_to control_path
+  end
 
   
 end
