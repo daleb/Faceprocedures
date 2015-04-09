e5e5282d97904741dd07d46e67a1dcb9f5ff5c68
Viji Kumar <vijikumar@carmatec.com>
2015-04-09 10:50:52 +0530

    for adding condition

--- app/controllers/participant_controller.rb
+++ app/controllers/participant_controller.rb
@@ -30,8 +30,7 @@ class ParticipantController < ApplicationController
       @page = "waiting"
       user_status = "On waiting screen"
     end
-    #raise [$user_data.select{|user|user[:status]=="Completed Quiz And Waiting"}.length , $user_count].inspect
-    if $user_data.select{|user|user[:status]=="Completed Quiz And Waiting"}.length == $user_count
+    if $user_count > 0 && $user_data.select{|user|user[:status]=="Completed Quiz And Waiting"}.length == $user_count
        @page = "statement"
        user_status = "On statement page"
     end
